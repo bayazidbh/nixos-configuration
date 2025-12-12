398e28a28ea38d8d4867da0b3e3e9f7ea5b352aa
Bayazid B. Halim <bayazid.bustami.halim@gmail.com>
2025-12-13 00:14:23 +0700

    add nix-alien to flake

--- bbh-ally-nixos/flake.nix
+++ bbh-ally-nixos/flake.nix
@@ -5,11 +5,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable"; # IMPORTANT
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     jovian.follows = "chaotic/jovian";
+    nix-alien.url = "github:thiagokokada/nix-alien";
   };
 
-  outputs = { nixpkgs, chaotic, jovian, nixos-hardware, ... }: {
+  outputs = { nixpkgs, nixos-hardware, chaotic, jovian, nix-alien ... }: {
     nixosConfigurations = {
       bbh-ally-nixos = nixpkgs.lib.nixosSystem { # Replace "hostname" with your system's hostname
         system = "x86_64-linux";
@@ -18,7 +19,12 @@
           ./hardware-configuration.nix
           nixos-hardware.nixosModules.asus-ally-rc71l
           jovian.nixosModules.default
-          chaotic.nixosModules.default # IMPORTANT
+          chaotic.nixosModules.default
+          ({ self, system, ... }: {
+            environment.systemPackages = with self.inputs.nix-alien.packages.${system}; [
+              nix-alien
+            ];
+          })
         ];
       };
     };
